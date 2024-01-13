fa814b51368b90f74ffaf98bcb2df2c184d66273
DejanKov2901 <127941319+DejanKov2901@users.noreply.github.com>
2024-01-13 11:41:00 +0100

    no brightfield images anymore

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from Processor import Processor
 import os
 import pandas as pd
 from GUI import NFATAnalysisGUI
+import sys
 
 def generate_sorted_filename_list(directory_path, condition):
     # Get a list of all files in the directory
@@ -26,12 +27,14 @@ def main():
     save_path = input_directory + '/results/'
     os.makedirs(save_path, exist_ok=True)
 
-    brightfield_files = generate_sorted_filename_list(input_directory, 'Brightfield')
+    # brightfield_files = generate_sorted_filename_list(input_directory, 'Brightfield')
     channel_405_files = generate_sorted_filename_list(input_directory, 'CF-405')  # Nucleus, DAPI staining
     channel_488_files = generate_sorted_filename_list(input_directory, 'CF-488')  # NFAT
 
-    for elem in list(zip(brightfield_files, channel_405_files, channel_488_files)):
-        image_processor = Processor(elem[0], elem[1], elem[2], save_path)
+    for elem in list(zip(channel_405_files, channel_488_files)):
+        print("now processing files:" + str(elem))
+
+        image_processor = Processor(elem[0], elem[1], save_path)
         nuclei_rois = image_processor.find_ROIs()
         image_processor.subtract_background_NFAT_image()
         image_processor.create_cell_images(nuclei_rois)
@@ -45,3 +48,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    sys.exit()
